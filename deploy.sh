39c939e6c93d1bdd83f8084d4080a281610ab20f
lucky-frontEnd <2318113474@qq.com>
2023-01-11 15:53:11 +0800

    feat:  修改config

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,19 @@
 #!/usr/bin/env sh
 
-# 忽略错误
+# 确保脚本抛出遇到的错误
 set -e
 
-# 构建
-npm run docs:build
+# 生成静态文件
+npm run build
 
-# 进入待发布的目录
-cd docs/.vitepress/dist
-
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
+# 进入生成的文件夹
+cd public
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# 如果部署到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# 如果是部署到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:MichstaBe1024/MichstaBlog.git master:gh-pages  
 
 cd -
